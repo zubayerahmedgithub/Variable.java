311763cab42396f9d3b2f05e1b3686ff7d7c7b56
zubayerahmedgithub <sajib.cu.apece06@gmail.com>
2024-03-13 11:38:28 -0400

    Initial commit

---
+++ src/Variable.java
@@ -0,0 +1,22 @@
+public class Variable {
+    public static void main(String[] args) {
+        // Declare and initialize variables
+        int x = 5;
+        double y = 3.14;
+        java.lang.String name = "Jhon doe";
+
+        // Print variable values
+        System.out.println("The value of x is: " + x);
+        System.out.println("The value of y is: " + y);
+        System.out.println("The name is: " + name);
+
+        // Perform arithmetic operations
+        int sum = x + 10;
+        double product = sum * y;
+
+        // Print results of arithmetic operations
+        System.out.println("The sum of x and 10 is: " + sum );
+        System.out.println("The product of sum and y is: " + product);
+
+    }
+}
